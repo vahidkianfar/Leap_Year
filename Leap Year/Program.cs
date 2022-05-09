094142e77ba59d435929783c9efd81989d9b3163
vahidkianfar <vahidkianfar@gmail.com>
2022-05-09 03:12:15 +0100

    Handle Exception!

--- Leap Year/Program.cs
+++ Leap Year/Program.cs
@@ -1,23 +1,24 @@
 ﻿
     using System;
-    namespace Leap_Year  
-    {  
-        class Program   
-        {  
-            public static void Main()  
-            {  
+
+    namespace Leap_Year
+    {
+        class Program
+        {
+            public static void Main()
+            {
                 Console.WriteLine("Enter Year : ");
                 try
                 {
                     var year = Convert.ToInt32(Console.ReadLine());
-                    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) Console.WriteLine("{0} is a Leap Year.", year);
+                    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                        Console.WriteLine("{0} is a Leap Year.", year);
                     else Console.WriteLine("{0} is not a Leap Year.", year);
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("Year should be a number.");
                 }
-                
-            }  
-        }  
-    }  
+            }
+        }
+    }
