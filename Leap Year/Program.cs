e231c842a8178975dcd66f416397be204f056f17
vahidkianfar <vahidkianfar@gmail.com>
2022-05-09 02:57:46 +0100

    Initial Commit

---
+++ Leap Year/Program.cs
@@ -0,0 +1,23 @@
+﻿
+    using System;
+    namespace Leap_Year  
+    {  
+        class Program   
+        {  
+            public static void Main()  
+            {  
+                Console.WriteLine("Enter Year : ");
+                try
+                {
+                    var year = Convert.ToInt32(Console.ReadLine());
+                    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) Console.WriteLine("{0} is a Leap Year.", year);
+                    else Console.WriteLine("{0} is not a Leap Year.", year);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Year should be a number.");
+                }
+                
+            }  
+        }  
+    }  
